47361a200546ece27064dd6da404c390cc411b28
Heliocrat <heliocrat@gmail.com>
2025-02-22 02:25:55 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 import solidJs from '@astrojs/solid-js';
 
 // https://astro.build/config
 export default defineConfig({
+    image: {
+        service: passthroughImageService(),
+    },
     integrations: [starlight({
         title: 'Heliocrat',
         social: {
